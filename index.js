2879f3c250ac4a442b719700aa42c538685d5362
Soham Datta <sohamdatta34@gmail.com>
2022-05-11 22:37:57 +0530

    safari sucks

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ audio.volume = 0.1;
 window.onload = function () {
     console.log("loaded");
     console.log("move mouse or tap to play if on mobile");
+    // const player = document.getElementById('audio-player');
+    const canPlayOpus = (typeof audio.canPlayType === 'function' && audio.canPlayType('audio/ogg; codecs="opus"') !== '');
+    const isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    const isIE = /*@cc_on!@*/false || !!document.documentMode;
+    const isEdge = !isIE && !!window.StyleMedia;
+
+    if (isSafari || isIE || isEdge || !canPlayOpus) {
+        console.log("rimch ");
+        audio = new Audio();
+        audio.autoplay = true;
+        audio.src = "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
+        audio.src = 'images/woahh.mp3';
+    } else { console.log("not rimch"); }
     document.body.addEventListener("mousemove", function () {
         audio.play()
     });
@@ -49,7 +62,7 @@ if (window.matchMedia("(pointer: coarse)").matches) {
         images[currentIndex].style.display = "none";
         currentIndex = (currentIndex + 1) % images.length
         images[currentIndex].style.display = "block";
-    }, 247);
+    }, 206.808);
 } else {
     var lastUpdate = new Date().getTime();
     var lastX = -1, lastY = -1;
